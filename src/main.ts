d2786b40f5f7083c3e8918e00231215017bb6274
Melanie Estévez <melaniestevez@hotmail.com>
2026-01-27 08:27:55 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // si usas cookies/sesión
+    credentials: true, 
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.useGlobalFilters(new GlobalHttpExceptionFilter());
