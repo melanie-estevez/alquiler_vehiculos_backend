3d1a420b2f786f4f5ea34e44da01c08d7d5a4057
Martin Borja <martinborja31611@gmail.com>
2026-01-30 22:01:10 -0500

    fix pagos

--- src/pagos/pagos.entity.ts
+++ src/pagos/pagos.entity.ts
@@ -1,19 +1,13 @@
-import {
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  ManyToOne,
-  JoinColumn,
-} from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
 import { Factura } from 'src/facturas/factura.entity';
 import { Reservas } from 'src/reservas/reservas.entity';
 
-@Entity('pagos')
+@Entity('pago')
 export class Pago {
   @PrimaryGeneratedColumn('uuid')
   id_pago: string;
 
-  @Column({ type: 'numeric', precision: 10, scale: 2 })
+  @Column('decimal', { precision: 10, scale: 2 })
   monto: number;
 
   @Column()
@@ -22,14 +16,14 @@ export class Pago {
   @Column()
   estado: string;
 
-  @Column({ type: 'timestamp', default: () => 'NOW()' })
+  @Column({ type: 'timestamp' })
   fecha_pago: Date;
 
-  @ManyToOne(() => Factura, (factura) => factura.pagos, { onDelete: 'CASCADE' })
+  @ManyToOne(() => Factura, factura => factura.pagos, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'id_factura' })
   factura: Factura;
 
-  @ManyToOne(() => Reservas, (reserva) => reserva.pagos, { onDelete: 'CASCADE' })
+  @ManyToOne(() => Reservas, reserva => reserva.pagos, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'id_reserva' })
   reserva: Reservas;
 }
